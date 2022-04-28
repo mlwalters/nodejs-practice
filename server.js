1cbebf5266b3a6a3f38f9857489eee532d502788
Maricar Walters <maricar.walters@wwt.com>
2022-04-28 17:00:03 -0500

    added first sample route and error handler

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.Server({
+        host: 'localhost',
+        port: 1234,
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (req, res) => {
+            return 'List of todos'
+        }
+    });
+
+    await server.start();
+    console.log('Server running on ', server.info.uri);
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+
+});
+
+init();
